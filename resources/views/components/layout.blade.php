34a2d4ef88c9a58d07a2da3955ce5ed6a5d7a617
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-10-28 11:40:23 +0000

    login and register markup

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{$title}}</title>
+    <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js"></script>
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body class="bg-[#121212] text-[#e6e6e6]">
+    {{$slot}}
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+  return view('home');
+})->middleware('auth');
+
+Route::get('/register', function () {
+  return view('register');
+})->name("register");
+
+Route::get('/login', function () {
+  return view('login');
+})->name("login");
